2dc5a66e8a21104af4ca0e9577634d2822706d64
E.Wolf <e.wolf@webmi.ru>
2020-12-27 21:15:12 +0300

    Добавлена черновая версия сохранения значений параметров ImageProcessor

--- app/Http/Controllers/API/ImageProcessorController.php
+++ app/Http/Controllers/API/ImageProcessorController.php
@@ -6,9 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ImageProcessorResource;
 use App\Http\Resources\IndexResource;
 use App\Models\ImageProcessor;
+use App\Models\ImageProcessorActionParamValue;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class ImageProcessorController extends Controller
 {
@@ -31,13 +34,37 @@ class ImageProcessorController extends Controller
 
     }
 
-    public function put(Request $request, int $id) {
+    public function put(Request $request, int $id)
+    {
         $model = ImageProcessor::find($id);
 
-        if($model) {
-            $model->update([
-                'title' => $request->get('title'),
-            ]);
+        DB::beginTransaction();
+
+        try {
+            if ($model) {
+                $model->update([
+                    'title' => $request->get('title'),
+                ]);
+            }
+
+            foreach ($request->get('actions', []) as $action) {
+                foreach ($action['params'] as $param) {
+                    if (mb_strlen($param['value'])) {
+                        ImageProcessorActionParamValue::updateOrCreate([
+                            'image_processor_action_param_id' => $param['id'],
+                            'image_processor_id' => $model->id,
+                        ],
+                        [
+                            'value' => $param['value'],
+                        ]);
+                    }
+                }
+            }
+
+            DB::commit();
+        } catch (Throwable $e) {
+            //@TODO Return error
+            DB::rollBack();
         }
 
         $model = ImageProcessor::with('actions.params', 'actionParamValues')->find($id);
